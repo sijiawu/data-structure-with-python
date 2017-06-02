c88db21837ab9f1b8af8aba4e657f728a988a9a9
Sijia Wu <swu36@hawk.iit.edu>
2017-06-02 14:34:32 -0500

    Insertion sort done

--- insertionSort.py
+++ insertionSort.py
@@ -1,18 +1,17 @@
-myList = [9,8,7,6,5,4,3,2,1]
+myList = [9,8,7,6,5,4,5,4,3,2,1]
 
 def insertionSort(myList):
-	marker = 0
-	while (marker < len(myList) - 1):
-		print("in")
+	marker = 1
+	while (marker < len(myList)):
+		for i in range(marker):
+			if(myList[i] > myList[marker]):
+				j = marker 
+				a = myList[j]
+				while(j > i):
+					myList[j] = myList[j-1]
+					j-=1
+				myList[i] = a
 		marker += 1
-		for i in range(marker-1):
-			print("mylist[i] is",myList[i],"mylist[marker]is",myList[marker])
-			if(myList[i] < myList[marker]):
-				print("yes?")
-				a = myList[i]
-				myList[i] = myList[marker]
-				myList[marker] = a
-			break
 	return myList
 
 print(insertionSort(myList))
